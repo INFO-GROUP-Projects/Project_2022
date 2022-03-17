5298bec28b376842c3ab9b800086d6102739cec9
TheGreatDevourer <anthonygould2304@outlook.com>
2022-03-17 19:50:22 +0000

    commit

--- App/config.py
+++ App/config.py
@@ -1,6 +1,5 @@
 DBURI = "postgres://yscpfqkikwaonb:77999dddcf3de3e60edabe01426c4b885b9b962644f635ead07f0fd03ec4478e@ec2-3-222-49-168.compute-1.amazonaws.com:5432/dc41o1pq2r2au"
 SQLITEDB = False
-SQLALCHEMY_DATABASE_URI = "sqlite:///temp-database.db"
 SECRET_KEY = "secret key"
 JWT_EXPIRATION_DELTA = 14
 ENV = "DEVELOPMENT"
