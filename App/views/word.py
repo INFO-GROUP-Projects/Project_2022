2e942bb562b42ca8e36d263226080a240b2f88f9
TheGreatDevourer <anthonygould2304@outlook.com>
2022-04-11 18:36:06 -0400

    commit

--- App/views/word.py
+++ App/views/word.py
@@ -21,20 +21,13 @@ def init_wordPage():
   return render_template('wordPageBegin.html',currentIndex =currentIndex ,incorrectWords = incorrectWords,
   correctWords = correctWords, currentScore = currentScore,  userStats = userStats )
 
-@word_views.route('/WordPage/<id>', methods={'POST'})
-def returnWordPage(id): 
+@word_views.route('/WordPage/', methods={'POST'})
+def returnWordPage(): 
   data = request.form
-  
   currentScore = data['currentScore']
   correctWords =  data['correctWords']
   incorrectWords =  data['incorrectWords']
   currentIndex =  data['index']
-
-  print('Hello')
-
-  if id > currentIndex:
-    return "Error"
-  
   cGame = currentGame()
   
   returnVar = getWordRand()
@@ -52,18 +45,27 @@ def getWordsId():
 @word_views.route('/api/validate/', methods = {'POST'})
 def validate_word():
     data = request.form
-    currentScore = data['currentScore']
-    correctWords =  data['correctWords']
-    incorrectWords =  data['incorrectWords']
-    currentIndex =  data['index']
-    points_gained = data['points']
+    currentScore = int(data['currentScore'])
+    correctWords =  int(data['correctWords'])
+    incorrectWords =  int(data['incorrectWords'])
+    currentIndex =  int(data['index'])
+    points_gained = int(data['points'])
     cGame = currentGame()
 
     if  data['spellingWord'] == data['userWord'] :
       flash('Correct')
-      currentScore = currentScore + points
-      correct_words = correct_words + 1
+      currentScore = currentScore + points_gained
+      correctWords = correctWords + 1
     else:
       flash('Incorrect')
-    currentIndex =  currentIndex + 1
-    return redirect('/WordPage/')
+      incorrectWords = incorrectWords + 1
+
+    returnVar = getWordRand()
+    spellWord = returnVar["word"]
+    uPoints = returnVar["points"]
+
+    return render_template('wordPage.html',cGame = cGame , spellWord = spellWord,uPoints =uPoints,
+  currentIndex =currentIndex ,incorrectWords = incorrectWords,correctWords = correctWords, 
+  currentScore = currentScore) 
+
+    #return redirect('/WordPage/')
