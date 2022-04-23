a255e4016359142ceb4983f7fad56ca64b8a10f4
Aliyah <Aliyahjohnx@gmail.com>
2022-04-23 18:17:49 +0000

    commit

--- App/models/stat.py
+++ App/models/stat.py
@@ -6,6 +6,7 @@ class Stat(db.Model):
     correct_words = db.Column(db.Integer, nullable=False)
     incorrect_words = db.Column(db.Integer, nullable=False)
     timeStarted = db.Column(db.DateTime, nullable=False)
+    gamemaster = db.Column(db.Gamemaster, nullable = False)
     id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False) # foreign key that links Stat to User Class
     user = db.relationship('User')
 
